1d511806e0cdbfdc180795e91deeff3151044407
qiuqilin <951686695@qq.com>
2020-09-08 15:14:12 +0800

    集成bugly

--- baselibrary/src/main/java/com/congda/baselibrary/app/IMSConfig.java
+++ baselibrary/src/main/java/com/congda/baselibrary/app/IMSConfig.java
@@ -7,12 +7,12 @@ import java.io.File;
 public class IMSConfig {
 
     public static final String BASE_URL="https://www.wanandroid.com/";
+
     public static final String BASE_VIDEO_URL="http://47.75.111.156/";
-//    public static final String BASE_URL="http://api.teliao.cc/tomato-app/";
-
+    //全局使用到的包名
     public static final String APP_PACKNAME = "com.rhby.edu.jianxin";
-
-    public static final String BUGLY_APPID = "c08e7834d0";
+    //bugly的AppId,直接替换bugly
+    public static final String BUGLY_APPID = "c08e783411";
     /**
      * (本带缓存路径)
      * mnt/sdcard/ 即为SD卡根路径
--- baselibrary/src/main/java/com/congda/baselibrary/app/IMSpUtil.java
+++ baselibrary/src/main/java/com/congda/baselibrary/app/IMSpUtil.java
@@ -3,4 +3,5 @@ package com.congda.baselibrary.app;
 public class IMSpUtil {
 
     public static final String FIRST_OPEN = "first_open";
+
 }
